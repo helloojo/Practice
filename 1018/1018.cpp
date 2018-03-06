c3b7fc03815d12c1a36f7b06804af86b73d781ca
rueng564 <rueng564@naver.com>
2018-03-06 13:02:49 +0900

    solve 15552

--- 1018/1018.cpp
+++ 1018/1018.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
-//BOJ #15552
+//BOJ #
+
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t;
-	cin >> t;
-	int a, b;
-	while (t--) {
-		cin >> a >> b;
-		cout << a + b<<'\n';
-	}
+	int n, m;
+	cin >> n >> m;
+
 	return 0;
 }
