d52b8ab12b9a8590108967288b55b1a0cbad698e
rueng564 <rueng564@naver.com>
2018-03-17 21:22:16 +0900

    solve 5032

--- 5032/5032.cpp
+++ 5032/5032.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
-//BOJ #
+//BOJ #5032
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	int e, f, c;
+	cin >> e >> f >> c;
+	int cur = e + f;
+	int ret = 0;
+	while (cur >= c) {
+		ret += cur / c;
+		cur = cur % c + cur / c;
+	}
+	cout << ret;
 	return 0;
 }
