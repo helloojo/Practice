c6fbe02232e85b578503759123d11bef53286bab
rueng564 <rueng564@naver.com>
2018-05-15 20:48:09 +0900

    solve 1182

--- 1182/1182.cpp
+++ 1182/1182.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 using namespace std;
 //BOJ #1182
+int n, s;
+int arr[21];
+
+int solve(int pos, int sum) {
+	int ret = 0;
+	if (sum == s) {
+		ret++;
+	}
+	for (int i = pos+1; i < n; i++) {
+		ret += solve(i, sum + arr[i]);
+	}
+	return ret;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n, s;
 	cin >> n >> s;
-	int arr[21];
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
+	int ret = 0;
+	for (int i = 0; i < n; i++) {
+		ret += solve(i, arr[i]);
+	}
+	cout << ret;
 }
