8e77a6b3b2a9d96fff2ef19387b7b2190808a711
rueng564 <rueng564@naver.com>
2018-05-15 20:30:42 +0900

    solve 1018

--- 1018/1018.cpp
+++ 1018/1018.cpp
@@ -51,6 +51,7 @@ int check(int y, int x) {
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	cout.tie(NULL);
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		cin >> map[i];
---
+++ 1182/1182.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//BOJ #1182
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, s;
+	cin >> n >> s;
+	int arr[21];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
