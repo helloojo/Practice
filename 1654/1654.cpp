861aefab8ed81f667dcf11c755e0fc98a089321b
rueng564 <rueng564@naver.com>
2018-05-16 21:54:07 +0900

    solve 1654

--- 1654/1654.cpp
+++ 1654/1654.cpp
@@ -2,16 +2,37 @@
 #include <algorithm>
 using namespace std;
 //BOJ #1654
-int lan[10001];
+typedef long long ull;
+ull lan[10001];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	int k, n;
-	int high = 0;
-	int low = 0;
+	cin >> k >> n;
+	ull high = 0;
+	ull low = 0;
 	for (int i = 0; i < k; i++) {
 		cin >> lan[i];
 		high = max(high, lan[i]);
 	}
+	high++;
+	ull mid = (low + high) / 2;
+	ull ret = 0;
+	while (low + 1 < high) {
+		ull sum = 0;
+		for (int i = 0; i < k; i++) {
+			sum += (lan[i] / mid);
+		}
+		if (sum < n) {
+			high = mid;
+		} else {
+			low = mid;
+		}
+		if (sum >= n) {
+			ret = max(ret, mid);
+		}
+		mid = (low + high) / 2;
+	}
+	cout << ret;
 }
