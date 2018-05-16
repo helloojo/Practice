99cfcc099765185874ec7e9a83f8ca398866261f
rueng564 <rueng564@naver.com>
2018-05-16 17:00:34 +0900

    solve 11921

--- 11921/11921.cpp
+++ 11921/11921.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 using namespace std;
 //BOJ #11921
-
-char buffer[5000001];
-
+const int MAXX = 1<<20;
+char buf[MAXX];
+int p = MAXX;
+inline char getcha() {
+	if (p == MAXX) {
+		fread(buf, 1, MAXX, stdin);
+		p = 0;
+	}
+	return buf[p++];
+}
+inline void getint(int& x) {
+	x = getcha()-'0';
+	int temp;
+	while ((temp=getcha())!='\n') {
+		x = x * 10 + (temp - '0');
+	}
+}
+typedef unsigned long long ull;
 int main() {
-	fread(buffer, 1, 5000001, stdin);
+	int n;
+	getint(n);
+	ull sum = 0ull;
+	int x;
+	printf("%d\n", n);
+	while (n--) {
+		getint(x);
+		sum += (ull)x;
+	}
+	printf("%llu",sum);
 	return 0;
 }
