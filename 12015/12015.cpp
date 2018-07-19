5950e470b1d990405ed6636bb97f6fcdb0835bbe
rueng564 <rueng564@naver.com>
2018-07-19 15:00:53 +0900

    solve 12015

---
+++ 12015/12015.cpp
@@ -0,0 +1,32 @@
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+//BOJ #12015
+
+vector<int> ret;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	ret.push_back(0);
+	int num;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (ret.back() < num) {
+			ret.push_back(num);
+		} else {
+			*lower_bound(ret.begin(), ret.end(), num) = num;
+		}
+	}
+	cout << ret.size() - 1;
+}
