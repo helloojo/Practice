a4e18d920f0799f6b278c4b98ac74975b9a3e1e4
rueng564 <rueng564@naver.com>
2018-07-30 15:35:25 +0900

    solve 1238

--- 1238/1238.cpp
+++ 1238/1238.cpp
@@ -6,47 +6,56 @@
 using namespace std;
 //BOJ #1238
 int n, m, x;
+vector<vector<pair<int, int>>> adj;
 int map[1001][1001];
 int dis[1001];
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-int dijkstra(int n) {
-	dis[n] = 0;
-	pq.push(make_pair(dis[n], n));
+int dijkstra(int src,int dst) {
+	pq.push({ 0,src });
 	while (!pq.empty()) {
 		auto p = pq.top();
 		pq.pop();
-		if (p.first > dis[p.second]) continue;
-		for (int i = 1; i <= n; i++) {
-			if (i != p.second && map[p.second][i]) {
-				int next = i;
-				int weight = p.first + map[p.second][i];
-				if (weight > dis[next]) continue;
-				dis[next] = weight;
-				pq.push(make_pair(weight, next));
+		if (dis[p.second] != 987654321) {
+			continue;
+		}
+		dis[p.second] = p.first;
+		int ads = adj[p.second].size();
+		for (int i = 0; i < ads; i++) {
+			int next = adj[p.second][i].second;
+			int weight = adj[p.second][i].first + p.first;
+			if (dis[next] == 987654321) {
+				pq.push({ weight,next });
 			}
 		}
 	}
-	return dis[x];
+	return dis[dst];
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cin >> n >> m >> x;
+	adj = vector<vector<pair<int, int>>>(n + 1);
 	int a, b, c;
 	for (int i = 0; i < m; i++) {
 		cin >> a >> b >> c;
-		map[a][b] = c;
+		adj[a].push_back({ c,b });
 	}
 	int ret = -1;
+	int ret1 = -1;
+	int ret2 = -1;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			dis[j] = 987654321;
 		}
-		ret = max(ret, dijkstra(i));
+		ret1 = dijkstra(i, x);
+		for (int j = 1; j <= n; j++) {
+			dis[j] = 987654321;
+		}
+		ret2 = dijkstra(x, i);
+		ret = max(ret, ret1 + ret2);
 	}
-	cout << ret * 2;
-
+	cout << ret;
 	return 0;
 }
