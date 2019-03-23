4e25222b4ecee7142db232b6e06d60b265959cf0
rueng564 <rueng564@naver.com>
2019-03-23 17:40:51 +0900

    solve 16212

---
+++ 16212/16212.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+//BOJ #16212
+typedef long long ll;
+typedef unsigned long long ull;
+int arr[500001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ' ';
+	}
+}
