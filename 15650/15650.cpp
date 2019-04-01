55157e584a006fd4dd468d7520d49ddf5f95e0d3
rueng564 <rueng564@naver.com>
2019-04-01 12:56:39 +0900

    solve 15650

---
+++ 15650/15650.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//BOJ #15650
+
+int arr[9];
+int n, m;
+void print() {
+	for (int i = 1; i <= m; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void pick(int level) {
+	if (level > m) {
+		print();
+		return;
+	}
+	for (int i = arr[level-1]+1; i <= n; i++) {
+		arr[level] = i;
+		pick(level + 1);
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	pick(1);
+	return 0;
+}
