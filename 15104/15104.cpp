52c26721237ea1be614f55024111bba8f0a3c10d
rueng564 <rueng564@naver.com>
2019-05-07 19:23:32 +0900

    solve 15104

--- 15104/15104.cpp
+++ 15104/15104.cpp
@@ -6,8 +6,36 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	string s;
+	string ss;
 	cin >> s;
-	if (s.size() % 2 == 1) {
+	int size = s.size();
+	ss.push_back(s[0]);
+	for (int i = 1; i < size; i++) {
+		ss.push_back('#');
+		ss.push_back(s[i]);
+	}
+	size = ss.size();
+	bool can = true;
+	for (int i = 0; i < size; i++) {
+		int left = i - 1;
+		int right = i + 1;
+		if (ss[i] == '#') {
+			int cnt = 0;
+			while (left >= 0 && right < size) {
+				if (ss[left] != ss[right]) {
+					break;
+				}
+				cnt += 2;
+				left--;
+				right++;
+			}
+			if (cnt) {
+				can = false;
+				break;
+			}
+		}
+	}
+	if (can) {
 		cout << "Odd.";
 	} else {
 		cout << "Or not.";
