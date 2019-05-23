b4f0e6e3c8512a0779ed4b2152a1457fb5125a6c
rueng564 <rueng564@naver.com>
2019-05-23 18:13:08 +0900

    solve 11066

--- 11066/11066.cpp
+++ 11066/11066.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 //BOJ #11066
 
 int file[501];
+int p[501];
 int memo[501][501];
-
+int solve(int i, int j) {
+	if (i == j) {
+		return 0;
+	}
+	if (memo[i][j]) {
+		return memo[i][j];
+	}
+	int ret = 987654321;
+	for (int k = i; k < j; k++) {
+		ret = min(ret, solve(i, k) + solve(k + 1, j) + p[j] - p[i - 1]);
+	}
+	return memo[i][j] = ret;
+}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int t;
-	cin>>t;
+	cin >> t;
 	int k;
-	while(t--) {
-		cin>>k;
-		for(int i = 0; i<k; i++) {
-			cin>>file[i];
+	while (t--) {
+		cin >> k;
+		memset(memo, 0, sizeof(memo));
+		for (int i = 1; i <= k; i++) {
+			cin >> file[i];
+			p[i] = p[i - 1] + file[i];
 		}
-		for(int i = 0; i<k; i++) {
-			for(int j = 0; j<k; j++) {
-
-			}
-		}
-		//쓴다, 안쓴다, memo해야할것 쓰거나 안쓴거 최소비용
-		//m(i,j) -> 파일이 
+		cout << solve(1, k) << '\n';
 	}
 	return 0;
 }
