a31afffdf4f0f4d7ee8ccde6687d3223648f321d
rueng564 <rueng564@naver.com>
2020-07-21 21:42:58 +0900

    solve count and say

---
+++ java/count_and_say/Solution.java
@@ -0,0 +1,33 @@
+package count_and_say;
+
+public class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String sayResult = countAndSay(n - 1);
+        int resultLength = sayResult.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        int prev = sayResult.charAt(0) - '0';
+        int cnt = 1;
+        for (int i = 1; i < resultLength; i++) {
+            int code = sayResult.charAt(i) - '0';
+            if (prev != code) {
+                stringBuilder.append(cnt).append(prev);
+                prev = code;
+                cnt = 0;
+            }
+            cnt++;
+        }
+        if (cnt != 0) {
+            stringBuilder.append(cnt).append(prev);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countAndSay(5));
+    }
+}
