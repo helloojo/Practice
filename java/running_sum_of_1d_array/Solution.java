15b076ff2419cbb80a3e625cb7ebc512eafcda96
rueng564 <rueng564@naver.com>
2020-07-21 21:43:10 +0900

    solve running sum of 1d array

---
+++ java/running_sum_of_1d_array/Solution.java
@@ -0,0 +1,13 @@
+package running_sum_of_1d_array;
+
+public class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] answer = new int[nums.length];
+        int numsLength = nums.length;
+        answer[0] = nums[0];
+        for (int i = 1; i < numsLength; i++) {
+            answer[i] = answer[i - 1] + nums[i];
+        }
+        return answer;
+    }
+}
