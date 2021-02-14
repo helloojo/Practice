c7cb568498a8c466e29ff5836cca58a675678285
rueng564 <rueng564@naver.com>
2021-02-14 22:38:22 +0900

    solve roman to integer

---
+++ java/roman_to_integer/Solution.java
@@ -0,0 +1,35 @@
+package roman_to_integer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    private final Map<Character, Integer> romanMap;
+
+    public Solution() {
+        romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+    }
+
+    public int romanToInt(String s) {
+        int size = s.length();
+        int i = size - 1;
+        int result = 0;
+        while (i >= 0) {
+            int num = romanMap.get(s.charAt(i));
+            if (result >= num * 5) {
+                result -= num;
+            } else {
+                result += num;
+            }
+            i--;
+        }
+        return result;
+    }
+}
