ab3abb4f9dcb07e4b909a4a0bb9f8cb473a6f29d
rueng564 <rueng564@naver.com>
2021-02-14 22:11:36 +0900

    solve palindrome number

---
+++ java/palindrome_number/Solution.java
@@ -0,0 +1,16 @@
+package palindrome_number;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int reverse = 0;
+        int origin = x;
+        while (x != 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        return reverse == origin;
+    }
+}
